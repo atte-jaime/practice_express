a1f754dba72e467c418900a24614ea051cf8cc32
tetolalo <jaimeal.99@hotmail.com>
2018-04-19 17:11:40 -0500

    Commit 3

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hola mundo");
+const express = require('express'),
+consolidate = require('consolidate');
+
+var app = express();
+
+app.get('/', function (req, res) {
+   res.send('Hello World'); 
+});
+
+app.listen(3000, function(){
+    console.log('Escuchando en el puerto 3000');
+});
